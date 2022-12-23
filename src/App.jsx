f3aa13459bd79f5592d2564ca5078e8d1490a7d7
Eding Muhamad <masaruedo@yahoo.com>
2022-12-23 08:01:19 +0700

    Creaet base Modal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import Modal from "./components/modal/Modal"
+
 function App() {
+  const [openModal, setOpenModal] = useState(false)
+  
   return (
     <>
       <h1>React Modal Tutorial</h1>
-      <button>Show modal</button>
+      <button onClick={() => setOpenModal(!openModal)}>Show modal</button>
+
+      <Modal isOpen={openModal} onClose={() => setOpenModal(false)}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque harum ea facere commodi neque nihil ratione eaque maiores quam odit laboriosam totam placeat veniam at, optio cupiditate autem labore! Reiciendis.</p>
+      </Modal>
     </>
   )
 }
---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,14 @@
+const Modal = ({ children, isOpen, onClose }) => {
+    return (isOpen && (
+        <div className="react-modal-overlay" onClick={onClose}>
+            <div className="react-modal-wrapper" onClick={e => e.stopPropagation()}>
+                <div className="react-modal-content">
+                    {children}
+                    <button type="button" onClick={onClose}>Close</button>
+                </div>
+            </div>
+        </div>
+    ))
+}
+
+export default Modal
